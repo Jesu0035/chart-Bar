6c767b704ed9f37de8555196b18a4b207e76eb81
Jesus lopez <jesu0035@stud.kea.dk>
2021-02-11 22:52:53 +0100

    first commit

---
+++ chartbar.js
@@ -0,0 +1,27 @@
+'use strict'
+
+window.addEventListener ('load', init);
+
+const model = []
+
+function init(){
+    console.log('init');
+}
+function loop(){
+    console.log('loop');
+
+}
+function getNumberOfCustomers(){
+    console.log('getNumber of the Customer');
+    return Math.floor(Math.random()*32 );
+
+    const queueSize = getNumberOfCustomers();
+    
+}
+function displayData(){
+    console.log('displayData');
+
+}
+function modifyModel(){
+
+}
